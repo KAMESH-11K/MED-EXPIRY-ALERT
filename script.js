8aecd21fa49d6a162dd3a668e1db634adb7bf924
KAMESH-11K <139991997+KAMESH-11K@users.noreply.github.com>
2023-08-04 18:49:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("clickButton").addEventListener("click", function () {
+    document.getElementById("message").textContent = "Button Clicked!";
+    window.location.href = "about.html";
+  });
